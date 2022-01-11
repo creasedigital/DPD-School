7b63e5761af9066b4e94a5af52d2bafdee2be4e3
creasedigital <ojorechris@gmail.com>
2022-01-11 15:09:48 +0100

    testing js

---
+++ JAVASCRIPT/app.js
@@ -0,0 +1,22 @@
+// const threeArr = [1, 4, 6];
+// threeArr.push('John', 'Thomas', ['Batholomew', 'Batshuayi']);
+// console.log(threeArr);
+// console.log(threeArr.pop());
+// console.log(threeArr);
+
+// Setup
+let sum = 0;
+
+function addThree() {
+	sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive() {
+	sum = sum + 5;
+}
+// Only change code above this line
+
+console.log(addThree());
+console.log(addFive());
